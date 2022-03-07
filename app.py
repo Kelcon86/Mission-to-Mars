57062a4ef5b4bb1d232c2fea1cc294eedc8c6a86
Kelly Conant <KellyC@MacBook-Air.local>
2022-03-06 20:30:14 -0600

    Adding files

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+
+
+import scraping
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
-import scraping
+
 
 app = Flask(__name__)
 
@@ -12,7 +15,7 @@ mongo = PyMongo(app)
 @app.route("/")
 def index():
     mars = mongo.db.mars.find_one()
-    return render_template("index.html", mars=mars)
+    return render_template("index1.html", mars=mars)
 
 
 @app.route("/scrape")
@@ -23,7 +26,5 @@ def scrape():
     return redirect('/', code=302)
 
 
-mars.update_one({}, {"$set": mars_data}, upsert=True)
-return redirect('/', code=302)
 if __name__ == "__main__":
     app.run()
